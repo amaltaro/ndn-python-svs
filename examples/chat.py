69fc95225d083643befff342d16ec0931829a89c
Alan Malta Rodrigues <alan.malta@cern.ch>
2023-11-28 20:57:32 -0500

    apply Jasons changes; call chat_gui as subprocess

--- examples/chat.py
+++ examples/chat.py
@@ -6,17 +6,24 @@
 #    @Documentation: https://ndn-python-svs.readthedocs.io
 
 # Basic Libraries
-import logging
 import subprocess
+import random
+import logging
 import sys
 from argparse import ArgumentParser, SUPPRESS
 from typing import List, Callable, Optional
 # NDN Imports
-from ndn.encoding import Name
 
+from ndn.encoding import Name
 # Custom Imports
 sys.path.insert(0, '.')
-from src.ndn.svs import SVSyncShared_Thread, SVSyncBase_Thread, SVSyncLogger, MissingData, AsyncWindow
+from src.ndn.svs import SVSyncShared_Thread, SVSyncLogger, MissingData, AsyncWindow
+import socket
+import threading
+
+
+HOST = "127.0.0.1"
+PORT = random.randint(50401, 50499)
 
 
 def parse_cmd_args() -> dict:
@@ -40,61 +47,100 @@ def parse_cmd_args() -> dict:
     return args
 
 
-def on_missing_data(thread: SVSyncBase_Thread) -> Callable:
+def on_missing_data(prog_instance, missing_list: List[MissingData]) -> None:
     taskwindow = AsyncWindow(10)
 
     async def wrapper(missing_list: List[MissingData]) -> None:
         async def missingfunc(nid: Name, seqno: int) -> None:
-            content_str: Optional[bytes] = await thread.getSVSync().fetchData(nid, seqno, 2)
+            content_str: Optional[bytes] = await prog_instance.svs_thread.getSVSync().fetchData(nid, seqno, 2)
             if content_str:
                 output_str: str = Name.to_str(nid) + ": " + content_str.decode()
-                sys.stdout.write("\033[K")
-                sys.stdout.flush()
-                print(output_str)
+                # Send output_str to the GUI client using the Program instance
+                prog_instance.send_to_client(output_str)
 
         for i in missing_list:
             while i.lowSeqno <= i.highSeqno:
                 taskwindow.addTask(missingfunc, (Name.from_str(i.nid), i.lowSeqno))
                 i.lowSeqno = i.lowSeqno + 1
 
-    return wrapper
+    wrapper(missing_list)
 
 
 class Program:
     def __init__(self, args: dict) -> None:
         self.args = args
         self.svs_thread: SVSyncShared_Thread = SVSyncShared_Thread(Name.from_str(self.args["group_prefix"]),
-                                                                   Name.from_str(self.args["node_id"]), on_missing_data,
+                                                                   Name.from_str(self.args["node_id"]),
+                                                                   lambda missing_list: on_missing_data(self,
+                                                                                                        missing_list),
                                                                    self.args["cache_data"])
         self.svs_thread.daemon = True
         self.svs_thread.start()
         self.svs_thread.wait()
         print(f'SVS chat client started | {self.args["group_prefix"]} - {self.args["node_id"]} |')
+        self.socket_thread = threading.Thread(target=self.start_socket_server, daemon=True)
+        self.socket_thread.start()
+        self.client_socket = None  # To keep track of the GUI client socket
 
-    def run(self) -> None:
-        while 1:
+    # def run(self) -> None:
+    #     while 1:
+    #         try:
+    #             val:str = input("")
+    #             sys.stdout.write("\033[F"+"\033[K")
+    #             sys.stdout.flush()
+    #             if val.strip() != "":
+    #                 print("YOU: "+val)
+    #                 self.svs_thread.publishData(val.encode())
+    #         except KeyboardInterrupt:
+    #             sys.exit()
+
+    def start_socket_server(self):
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server.bind(('127.0.0.1', PORT))
+        server.listen()
+        print(f'server listening on: {HOST}:{PORT}')
+        while True:
+            client, address = server.accept()
+            self.client_socket = client
+            client_handler = threading.Thread(target=self.handle_client, args=(client,))
+            client_handler.start()
+
+    def handle_client(self, client_socket):
+        while True:
             try:
-                val: str = input("")
-                sys.stdout.write("\033[F" + "\033[K")
-                sys.stdout.flush()
-                if val.strip() != "":
-                    print("YOU: " + val)
-                    self.svs_thread.publishData(val.encode())
-            except KeyboardInterrupt:
-                sys.exit()
+                message = client_socket.recv(1024).decode('utf-8')
+                if message:
+                    if message.strip() != "":
+                        print("YOU: " + message)
+                        self.svs_thread.publishData(message.encode())
+            except (ConnectionResetError, BrokenPipeError):
+                self.client_socket = None
+                break
+
+    def send_to_client(self, message: str):
+        if self.client_socket:
+            try:
+                self.client_socket.sendall(message.encode('utf-8'))
+            except BrokenPipeError:
+                self.client_socket = None
 
 
 def main() -> int:
     args = parse_cmd_args()
+    args["node_id"] = Name.to_str(Name.from_str(args["node_id"]))
+    args["group_prefix"] = Name.to_str(Name.from_str(args["group_prefix"]))
     args["cache_data"] = True
-
-    # setup gui
-    print("Creating the GUI...")
-    p = subprocess.Popen(['python3', 'examples/chat_gui.py'])
-
     SVSyncLogger.config(True, None, logging.DEBUG)
+
     prog = Program(args)
-    prog.run()
+    ###### Setup GUI
+    print("Creating the GUI...")
+    p = subprocess.Popen(['python3', 'examples/chat_gui.py', str(PORT)])
+    try:
+        threading.Event().wait()
+    except KeyboardInterrupt:
+        sys.exit()
 
     return 0
 
--- examples/chat_gui.py
+++ examples/chat_gui.py
@@ -1,105 +1,54 @@
-import argparse
-import json
-import sys
 import tkinter as tk
+import socket
+import threading
+import sys
 
 
-def parse_cmd_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description="GUI for an SVS Chat application")
-    parser.add_argument("-n", "--nodename", action="store", dest="node_name",
-                        required=True, help="id of this node in svs")
-    parser.add_argument("-gp", "--groupprefix", action="store", dest="group_prefix",
-                        required=True, help="overrides config | routable group prefix to listen from")
-    args = parser.parse_args()
-    return args
+HOST = "127.0.0.1"
+PORT = int(sys.argv[1])  # this is supposed to be the port number
 
 
-class Chat_App(tk.Tk):
-
-    def __init__(self, node_name, group_prefix):
-        """Start a very basic tkinter window"""
-        super().__init__()
-        self.node_name = node_name
-        self.group_prefix = group_prefix
-
-        # set up the window title
-        self.title("NDN SVS Chat application.")
-
-        # basic configuration
-        tk.Message(self, text=f"Group prefix: {group_prefix}", width=50).grid(row=0, column=0)
-        tk.Message(self, text=f"Node name:    {node_name}", width=50).grid(row=1, column=0)
-
-        # utilitarian variables
-        self.node_input_file = f"{node_name}_input.json"
-        self.node_output_file = f"{node_name}_output.json"
-        self.last_msg = dict()  # contains the latest message received through SVS Sync
-
-        # now clear files to be used
-        self._clear_files()
-
-    def _clear_files(self):
-        """Clear files to be used as a pipeline"""
-        with open(self.node_input_file, "w+") as jo:
-            json.dump({}, jo)
-        with open(self.node_output_file, "w+") as jo:
-            json.dump({}, jo)
-
-    def __call__(self, *args, **kwargs):
-        """Create objects of the root window and set actions"""
-        self.chat_msg = tk.Text(self, width=50)
-        self.chat_msg.grid(row=2, column=0, padx=10, pady=10)
-
-        self.user_msg = tk.Entry(self, width=50)
-        self.user_msg.grid(row=3, column=0, padx=10, pady=10)
-        self.user_msg.insert(tk.END, "Your message")
-
-        self.send_msg = tk.Button(self, text="Send", width=20, command=self.send_message)
-        self.send_msg.grid(row=4, column=0, padx=10, pady=10)
-        # callback function for when the user hits return
-        # self.send_msg.bind('<Key-Return>', self.send_message)
-
-    def send_message(self):
-        """Send message to the group and to the chat frame"""
-        user_msg = self.user_msg.get()
-        msg = f"\n{self.node_name}: {user_msg}"
-        self.chat_msg.insert(tk.END, msg)
-        # now write to the file
-        self._send_message(user_msg)
-
-    def _send_message(self, message):
-        """Actually write to the file pipeline"""
-        with open(self.node_output_file, "wt") as jo:
-            json.dump({"node_name": self.node_name, "message": message}, jo)
-
-    def recv_message(self):
-        """Receive messages from the group and write to the chat frame"""
-        recv_msg = self._recv_message()
-        if recv_msg:
-            msg = f"\n{recv_msg['node_name']}: {recv_msg['message']}"
-            self.chat_msg.insert(tk.END, msg)
-
-    def _recv_message(self):
-        """Receive messages from the group and write to the chat frame"""
-        with open(self.node_input_file, "rt") as jo:
-            data = json.load(jo)
-        if data == self.last_msg:
-            return {}
-        else:
-            if "node_name" not in data or "message" not in data:
-                print(f"ERROR: received data in a wrong format. Data is: {data}")
-                return {}
-            else:
-                return data
+def listen_for_messages():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.connect((HOST, PORT))
+        while True:
+            message = server_socket.recv(1024).decode('utf-8')
+            if message:
+                root.after(0, lambda: display_message(f"Other: {message}"))
 
 
-def main() -> int:
-    args = parse_cmd_args()
+def send_message():
+    message = message_entry.get()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect((HOST, PORT))
+        client_socket.sendall(message.encode('utf-8'))
+    display_message(f"You: {message}")
+    message_entry.delete(0, tk.END)
 
-    chat_app = Chat_App(args.node_name, args.group_prefix)
-    chat_app()
-    chat_app.mainloop()
-    return 0
 
+def display_message(message):
+    chat_history.config(state=tk.NORMAL)
+    chat_history.insert(tk.END, message + "\n")
+    chat_history.config(state=tk.DISABLED)
+    chat_history.yview(tk.END)
 
-if __name__ == "__main__":
-    sys.exit(main())
+# Creating the main window
+root = tk.Tk()
+root.title("Chat App")
+
+# Creating the chat history text box
+chat_history = tk.Text(root, state=tk.DISABLED)
+chat_history.pack(padx=10, pady=10)
+
+# Creating the message entry field
+message_entry = tk.Entry(root)
+message_entry.pack(padx=10, pady=10, fill=tk.X)
+
+# Creating the send button
+send_button = tk.Button(root, text="Send", command=send_message)
+send_button.pack(padx=10, pady=10)
+
+# Start listening for messages
+threading.Thread(target=listen_for_messages, daemon=True).start()
+
+root.mainloop()
